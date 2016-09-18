7ed371aefa81d75156eaa12f2449af993e76bf37
AnimA09 <quererespoder_91@hotmail.com>
2016-09-18 23:22:09 +0200

    -Imagen añadida -TextView añadido -Añadido 2 strings -Button añadido

--- app/src/main/java/com/example/anima/aboutrobot2016/MainRobot.java
+++ app/src/main/java/com/example/anima/aboutrobot2016/MainRobot.java
@@ -2,12 +2,38 @@ package com.example.anima.aboutrobot2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainRobot extends AppCompatActivity {
+
+    private Button btn;
+    private TextView txt;
+    private int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_robot);
+
+        btn = (Button) findViewById(R.id.btn);
+        txt = (TextView) findViewById(R.id.txt);
+
+        contador = 0;
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                contador++;
+
+                txt.setText(R.string.txt2);
+
+                if (contador > 0){
+                    btn.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }

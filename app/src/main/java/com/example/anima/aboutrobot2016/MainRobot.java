b3650ab57a571604b3a946222a461940d3e10994
AnimA09 <Barbara9>
2016-09-22 17:49:56 +0200

    -Cambia de imagen si haces 1 click -El boton desaparece si hace 2º click

--- app/src/main/java/com/example/anima/aboutrobot2016/MainRobot.java
+++ app/src/main/java/com/example/anima/aboutrobot2016/MainRobot.java
@@ -2,16 +2,20 @@ package com.example.anima.aboutrobot2016;
 
 import android.app.Activity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class MainRobot extends Activity {
 
     private Button btn;
     private TextView txt;
     private int contador;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class MainRobot extends Activity {
 
         btn = (Button) findViewById(R.id.btn);
         txt = (TextView) findViewById(R.id.txt);
+        img = (ImageView) findViewById(R.id.img);
 
         contador = 0;
 
@@ -29,9 +34,14 @@ public class MainRobot extends Activity {
             public void onClick(View view) {
                 contador++;
 
-                txt.setText(R.string.txt2);
+                if(contador == 1){
+                    img.setImageResource(R.drawable.robot2);
+                    txt.setText(R.string.txt2);
+                    btn.setText(R.string.btn2);
+                }
 
-                if (contador > 0){
+                if(contador == 2){
+                    txt.setText(R.string.txt3);
                     btn.setVisibility(View.INVISIBLE);
                 }
             }
